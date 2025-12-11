225aaa26943c8b18facdab0fd9ce0f8e0f860006
Rahulx0 <rahulsharma1234h@gmail.com>
2025-12-11 12:22:37 +0530

    changed some resource

--- backend/backend.tf
+++ backend/backend.tf
@@ -37,7 +37,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
 }
 
 resource "aws_dynamodb_table" "dynamodb_lock_table" {
-    name = "devops-automation-project-demo-lock-table-11122334-utkarsh"
+    name = "devops-automation-project-lock-table-rahul"
     billing_mode = "PAY_PER_REQUEST"
     hash_key = "LockID"
 
