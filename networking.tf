3186b77600c9a89b35ad9252c60a21b49ce114be
Rahulx0 <rahulsharma1234h@gmail.com>
2025-12-11 12:24:57 +0530

    made some changes

--- networking.tf
+++ networking.tf
@@ -4,10 +4,7 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
+  
   }
 
   backend "s3" {
