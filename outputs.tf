85364fef117a984fb4e4d44a42edae847616f7a9
Rahulx0 <rahulsharma1234h@gmail.com>
2025-12-15 11:19:49 +0530

    made changes

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,7 @@
 output "instance_public_ip" {
-  description = "The public IP address of the EC2 instance."
-  value       = aws_instance.web_server[0].public_ip
+  value = aws_instance.web_server[0].public_ip
+}
+
+output "instance_id" {
+  value = aws_instance.web_server[0].id
 }
